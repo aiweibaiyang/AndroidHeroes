3021e788e3db38d5d4c9f90c9b03eabc0e72ae3c
Tony <tony@gmail.com>
2019-10-20 16:39:01 +0800

    更新NO4

---
+++ NO4/MyListView/app/src/main/java/com/example/mylistview/NotifyAdapter.java
@@ -0,0 +1,67 @@
+package com.example.mylistview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by 25400 on 2019/10/20.
+ */
+
+public class NotifyAdapter extends BaseAdapter {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+
+    public NotifyAdapter(Context context, List<String> data) {
+        this.mData = data;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        //判断是否缓存
+        if (convertView == null){
+            holder = new ViewHolder();
+            //通过LayoutInflater实例化布局
+            convertView = mInflater.inflate(R.layout.notify_item, null);
+            holder.img = (ImageView) convertView.findViewById(R.id.imageView);
+            holder.title = (TextView) convertView.findViewById(R.id.textView);
+            convertView.setTag(holder);//将ViewHolder存储到View中
+        }else {
+            //通过tag找到缓存的布局\
+            holder = (ViewHolder) convertView.getTag();
+        }
+        //设置布局中控件要显示的视图
+        holder.img.setBackgroundResource(R.drawable.ic_launcher);
+        holder.title.setText(mData.get(position));
+        return convertView;
+    }
+
+    private final class ViewHolder{
+        public ImageView img;
+        public TextView title;
+    }
+}
---
+++ NO4/MyListView/app/src/main/java/com/example/mylistview/NotifyTest.java
@@ -0,0 +1,42 @@
+package com.example.mylistview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 25400 on 2019/10/20.
+ */
+
+public class NotifyTest extends Activity {
+
+    private List<String> mData;
+    private ListView mListView;
+    private NotifyAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notify);
+        mData = new ArrayList<String>();
+        for (int i = 0; i < 20; i++){
+            mData.add(""+i);
+        }
+        mListView = (ListView) findViewById(R.id.listView);
+        mAdapter = new NotifyAdapter(this, mData);
+        mListView.setAdapter(mAdapter);
+        for (int i = 0; i < mListView.getChildCount(); i++){
+            View view = mListView.getChildAt(i);
+        }
+    }
+
+    public void btnAdd(View view){
+        mData.add("new");
+        mAdapter.notifyDataSetChanged();
+        mListView.setSelection(mData.size() - 1);
+    }
+}
