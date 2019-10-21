be7e6041fba569ceb6a27f18d8ecce855c859e81
Tony <tony@gmail.com>
2019-10-21 22:22:21 +0800

    实现滑动-scrollBy方法

---
+++ NO5/DragViewTest/app/src/main/java/com/example/dragviewtest/DragView4.java
@@ -0,0 +1,55 @@
+package com.example.dragviewtest;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by 25400 on 2019/10/21.
+ */
+
+public class DragView4 extends View {
+
+    private int lastX;
+    private int lastY;
+
+    public DragView4(Context context) {
+        super(context);
+        ininView();
+    }
+
+    public DragView4(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        ininView();
+    }
+
+    public DragView4(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        ininView();
+    }
+
+    private void ininView() {
+        setBackgroundColor(Color.GRAY);
+    }
+
+    //scrollBy方法
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = (int) event.getX();
+                lastY = (int) event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int offsetX = x - lastX;
+                int offsetY = y - lastY;
+                ((View) getParent()).scrollBy(-offsetX, -offsetY);
+                break;
+        }
+        return true;
+    }
+}
