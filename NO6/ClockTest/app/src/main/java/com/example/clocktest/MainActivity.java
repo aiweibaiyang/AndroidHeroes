0f0c414a3f96a011b36bc235064308218b5fd9ee
Tony <tony@gmail.com>
2019-10-23 16:17:15 +0800

    6.4.1Canvas和6.4.2Layer图层

--- NO6/ClockTest/app/src/main/java/com/example/clocktest/MainActivity.java
+++ NO6/ClockTest/app/src/main/java/com/example/clocktest/MainActivity.java
@@ -1,19 +1,13 @@
-package com.example.imageshape;
+package com.example.clocktest;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void btnImageMatrix(View view) {
-        startActivity(new Intent(this, ImageMatrixTest.class));
+        setContentView(new Clock(this));
     }
 }
