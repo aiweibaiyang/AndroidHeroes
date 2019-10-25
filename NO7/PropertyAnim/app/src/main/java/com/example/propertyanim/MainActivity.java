6124d6d553230c285cb0b8e1b422dce05d95c64d
Tony <tony@gmail.com>
2019-10-25 20:49:03 +0800

    7.2 完结

--- NO7/PropertyAnim/app/src/main/java/com/example/propertyanim/MainActivity.java
+++ NO7/PropertyAnim/app/src/main/java/com/example/propertyanim/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.propertyanim;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationSet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +23,28 @@ public class MainActivity extends AppCompatActivity {
         animator.setDuration(300);
         animator.start();
     }
+
+    public void btnPropertyValuesHolder(View view){
+        PropertyValuesHolder pvh1 = PropertyValuesHolder.ofFloat("translationX", 300f);
+        PropertyValuesHolder pvh2 = PropertyValuesHolder.ofFloat("scaleX", 1f, 0, 1f);
+        PropertyValuesHolder pvh3 = PropertyValuesHolder.ofFloat("scaleY", 1f, 0, 1f);
+        ObjectAnimator.ofPropertyValuesHolder(view, pvh1, pvh2, pvh3).setDuration(1000).start();
+    }
+
+    public void btnAnimatorSet(View view){
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(view, "translationX", 300f);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(view, "scaleX", 1f, 0, 1f);
+        ObjectAnimator animator3 = ObjectAnimator.ofFloat(view, "scaleY", 1f, 0, 1f);
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(1000);
+        set.playTogether(animator1, animator2, animator3);
+        set.start();
+    }
+
+//    public void btnScaleX(View view){
+//        Animator anim = AnimatorInflater.loadAnimator(this, R.animator.scale);
+//        anim.setTarget(mMv);
+//        anim.start();
+//    }
 
 }
