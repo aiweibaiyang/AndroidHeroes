2e0dc92464609b0fc4ec6d3cde312d1abfb5ae92
Tony <tony@gmail.com>
2019-10-28 20:03:25 +0800

    7.6 Android 5.X SVG矢量动画机制

---
+++ NO7/VectorTest/app/src/main/java/com/example/vectortest/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.vectortest;
+
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.image);
+        imageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                animate();
+            }
+        });
+    }
+
+    private void animate(){
+        Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof Animatable){
+            ((Animatable) drawable).start();
+        }
+    }
+}
